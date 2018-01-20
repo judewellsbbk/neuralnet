662054399ed1ad6cfb703ca0d9302f5c07f487f0
judewellsbbk <jwells03@dcs.bbk.ac.uk>
2018-01-20 13:28:00 +0000

    more comments

--- nn.py
+++ nn.py
@@ -3,13 +3,20 @@ prods = pd.read_csv('product.csv')
 print(prods.head())
 
 #=====STEPS=====
+
 #=FORWARD PROPAGATION=
 #we want to run the model on each set of inputs and calculate the 'error':
 #Take each row split into input a, input b and target.
 #Run a & b through the nodes to calculate the output.
 #Each node is calculated by taking the sum of inputs * weights from previous layer
+
+#=CALCULATE ERROR=
 #Calculate the error for each row (pair of inputs) by calculating output and subtracting from target
 #Store a list of errors in a list
 #Calculate mean squared error for all individual errors
+
 #=BACK PROPAGATION=
 #Calculate the slope of loss function at the output and adjust weights by weight - (slope * learning rate)
+#Repeat this process going back towards the input adjusting the weights at each layer.
+
+#After all weights have been adjusted repeat process of forward propagation, calculate error  and back propagation until slope = 0
